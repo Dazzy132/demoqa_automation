6227d022b733f5de7461478568b7a7da6ca8a5c1
Dazzy132 <vladsolo23rus@gmail.com>
2024-04-20 18:41:09 +0300

    Add negative test

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 
 from data.generator import generated_textbox_data
 from pages.elements_page import TextBoxPage
@@ -20,3 +21,20 @@ class TestElements(BaseTest):
             text_box_page.fill_text_box_fields(text_box_input_data)
 
             assert text_box_input_data == text_box_page.check_output_textbox_data()
+
+        @allure.title("Test incorrect email")
+        @pytest.mark.parametrize(
+            "email", ["email", "@domain.com"]
+        )
+        def test_incorrect_email(self, driver, email):
+            """Проверка на не корректный email"""
+            text_box_page = TextBoxPage(driver, url="https://demoqa.com/text-box")
+            text_box_page.open()
+
+            text_box_input_data = next(generated_textbox_data())
+            text_box_input_data.email = email
+            text_box_page.fill_text_box_fields(text_box_input_data)
+
+            assert text_box_page.find_element(
+                ("xpath", "//input[@id='userEmail' and contains(@class, 'field-error')]")
+            ).is_displayed()
